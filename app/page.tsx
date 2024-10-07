1f3ea3b363fb5b54a82108ae8e449126f3a16cce
DavNej <david.nejar@gmail.com>
2024-10-07 10:46:45 +0200

    setup tanstack-query

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import * as React from 'react'
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
+import { getQueryClient } from '@/lib/get-query-client'
+
+export default async function Home() {
+  const queryClient = getQueryClient()
+
   return (
-    <main className="p-8 text-center">
-      <h2>Welcome to TX Manager</h2>
-    </main>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <main className="p-8 text-center">
+        <h2>Welcome to TX Manager</h2>
+      </main>
+    </HydrationBoundary>
   )
 }
